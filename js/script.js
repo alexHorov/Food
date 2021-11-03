2a7f32e5d7ae9c784776a01fd095ec5c18b36934
AlexHorovyi <alexhorovyi@gmail.com>
2021-11-03 23:26:17 +0200

    finish with tabs

--- js/script.js
+++ js/script.js
@@ -9,7 +9,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function hideTabContent() {
         tabsContent.forEach(item => {
-            item.style.display = 'none';
+            // item.style.display = 'none';
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+
         });
 
         tabs.forEach(item => {
@@ -20,7 +23,9 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     function showTabContent(i = 0) {
-        tabsContent[i].style.display = 'block';
+        // tabsContent[i].style.display = 'block';
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
         tabs[i].classList.add('tabheader__item_active');
     };
 
@@ -29,7 +34,13 @@ window.addEventListener('DOMContentLoaded', () => {
     tabParent.addEventListener('click', (event) => {
         let target = event.target;
         if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
 
+            });
         }
 
     })
