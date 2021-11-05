0019fe55f0c2edcdc65b34619a68c821b54759b5
Горовой Александр <a.gorovoy@cosmo.mm>
2021-11-05 08:41:20 +0200

    merdge after timer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', () => {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
+    // TABS
     const tabs = document.querySelectorAll('.tabheader__items'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
+
     console.log(tabsContent);
     console.log(tabsParent);
 
@@ -28,68 +30,50 @@ window.addEventListener('DOMContentLoaded', () => {
     tabsParent.addEventListener('click', (event) => {
         const target = event.target;
 
-=======
-=======
+        const tabs = document.querySelectorAll('.tabheader__item'),
+            tabsContent = document.querySelectorAll('.tabcontent'),
+            tabParent = document.querySelector('.tabheader__items');
 
-
-    // TABS
-
-
->>>>>>> main
-    const tabs = document.querySelectorAll('.tabheader__item'),
-        tabsContent = document.querySelectorAll('.tabcontent'),
-        tabParent = document.querySelector('.tabheader__items');
-
-    function hideTabContent() {
-        tabsContent.forEach(item => {
-            // item.style.display = 'none';
-            item.classList.add('hide');
-            item.classList.remove('show', 'fade');
-
-        });
-
-        tabs.forEach(item => {
-            item.classList.remove('tabheader__item_active');
-
-        });
-
-    };
-
-    function showTabContent(i = 0) {
-        // tabsContent[i].style.display = 'block';
-        tabsContent[i].classList.add('show', 'fade');
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabheader__item_active');
-    };
-
-    hideTabContent();
-    showTabContent();
-    tabParent.addEventListener('click', (event) => {
-        let target = event.target;
->>>>>>> main
-        if (target && target.classList.contains('tabheader__item')) {
-            tabs.forEach((item, i) => {
-                if (target == item) {
-                    hideTabContent();
-                    showTabContent(i);
-                }
-<<<<<<< HEAD
-            });
-        }
-    });
-
-});
-=======
+        function hideTabContent() {
+            tabsContent.forEach(item => {
+                item.style.display = 'none';
+                // item.classList.add('hide');
+                // item.classList.remove('show', 'fade');
 
             });
+
+            tabs.forEach(item => {
+                item.classList.remove('tabheader__item_active');
+
+            });
+
         }
 
-<<<<<<< HEAD
-    })
-})
->>>>>>> main
-=======
+        function showTabContent(i = 0) {
+            tabsContent[i].style.display = 'block';
+            // tabsContent[i].classList.add('show', 'fade');
+            // tabsContent[i].classList.remove('hide');
+            tabs[i].classList.add('tabheader__item_active');
+        }
+
+        hideTabContent();
+        showTabContent();
+        tabParent.addEventListener('click', (event) => {
+            let target = event.target;
+
+            if (target && target.classList.contains('tabheader__item')) {
+                tabs.forEach((item, i) => {
+                    if (target == item) {
+                        hideTabContent();
+                        showTabContent(i);
+                    }
+
+                });
+            }
+        });
+
     });
+
 
 
     // TIMER 
@@ -146,4 +130,3 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     setCloсk('.timer', deadline);
 });
->>>>>>> main
