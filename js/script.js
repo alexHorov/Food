6db4abdca7b47c378508eea06609e9f314a465c9
Горовой Александр <a.gorovoy@cosmo.mm>
2021-11-17 16:27:19 +0200

    POST problem

--- js/script.js
+++ js/script.js
@@ -260,27 +260,41 @@ window.addEventListener('DOMContentLoaded', () => {
     //// FORMS
 
     const forms = document.querySelectorAll('form');
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы свами свяжемся',
+        failure: 'Что-то пошло не так'
+    };
+    forms.forEach(item => {
+        postData(item);
+    });
 
     function postData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
             const request = new XMLHttpRequest();
             request.open('POST', 'server.php');
 
-            request.setRequestHeader('Content-type', 'multipart/form-data');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 
-            const formData = new formData(form);
+            const formData = new FormData(form);
 
             request.send(formData);
             request.addEventListener('load', () => {
                 if (request.status === 200) {
-                    console.log(request.response)
+                    // console.log(request.response);
+                    statusMessage.textContent = message.success;
+                } else {
+                    statusMessage.textContent = message.failure;
                 }
-            })
-        })
+            });
+        });
     }
-
-}
 
 });
